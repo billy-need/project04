8cedd1b4c7c540e4e89522d8e7aacaba29cabb7d
Mike <mzeno184@gmail.com>
2020-10-09 16:48:22 -0400

    Created basic home page structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # project04
 Web Development Project 04 Trader App
+
+Home Page:
+    User's account balance
+
+    Stock search | display current stock info
+
+    Links to Buy/Sell Stock and Portfolio
+
--- trader/models.py
+++ trader/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# need an Account model: id, username, password, balance
+    #this is how you login and needs to update when sell or buy stock
+# need a Stock model: id, symbol, name, shares (entered by user), price (calculated from info on finance), orderDate 
