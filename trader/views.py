93db206cb7c0bfc22e4a7a2df035e19c65f81418
Mike <mzeno184@gmail.com>
2020-10-10 23:02:42 -0400

    Added sell functionality

--- trader/views.py
+++ trader/views.py
@@ -1,5 +1,5 @@
 from trader.models import Stock
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_list_or_404, get_object_or_404, render, redirect
 from .forms import StockForm, TickerForm
 from .models import Account, Stock
 
@@ -23,11 +23,12 @@ def stock(request):
     account = get_object_or_404(Account, pk = 1)
     balance = account.balance
     username = account.userName
+    stocks = get_list_or_404(Stock)
     if request.method == "POST":
         form = StockForm(request.POST)
         if form.is_valid(): # look up error codes for is_valid
             price = form.cleaned_data['price']
-            #symbol = form.cleaned_data['symbol']
+            symbol = form.cleaned_data['symbol']
             shares = form.cleaned_data['shares']
             action = request.POST['submit']
             if action == "Buy":
@@ -36,8 +37,22 @@ def stock(request):
                     form.save();
                 else:
                     print('Error: Insufficent Funds') # create an error message screen
-            else:
-                account.balance = balance + (price * shares)
+            if action == "Sell":
+                # account.balance = balance + (price * shares)
+                for stock in stocks:
+                    if stock.symbol == symbol:
+                        if stock.shares > shares:
+                            account.balance = balance + (price * shares)
+                            stock.shares = stock.shares - shares
+                            stock.save()
+                        if stock.shares == shares:
+                            account.balance = balance + (price * shares)
+                            stock.delete()
+                        else:
+                            print('Error: Cannot sell more than you own')
+                        break 
+                    else:
+                        print('Error: Cannot find stock symbol')
             account.save();
             return redirect('/stock')
     form = StockForm()
